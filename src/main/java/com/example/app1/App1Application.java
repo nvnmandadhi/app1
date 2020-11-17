551ca87cee8dee9c9967187366282ba38130b472
naveenmandadhi <N@v33nkumar1786>
2020-11-16 20:07:59 -0600

    init

---
+++ src/main/java/com/example/app1/App1Application.java
@@ -0,0 +1,38 @@
+package com.example.app1;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class App1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App1Application.class, args);
+    }
+
+}
+
+@RestController
+class HelloController {
+
+    @GetMapping
+    public AppData getAppData() {
+        final AppData appData = new AppData();
+        appData.setDate(new Date());
+        appData.setVersion("v1");
+        return appData;
+    }
+}
+
+@Getter
+@Setter
+class AppData {
+    private Date date;
+    private String version;
+}
