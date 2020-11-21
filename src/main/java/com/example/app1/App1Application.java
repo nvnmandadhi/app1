75be0157e23c11705e168c41c66fa41fbc06bea8
naveenmandadhi <N@v33nkumar1786>
2020-11-20 22:21:39 -0600

    add env to recognize deployment

--- src/main/java/com/example/app1/App1Application.java
+++ src/main/java/com/example/app1/App1Application.java
@@ -2,6 +2,7 @@ package com.example.app1;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,11 +22,15 @@ public class App1Application {
 @RestController
 class HelloController {
 
+    @Value("${env:'NA'}")
+    private String env;
+
     @GetMapping
     public AppData getAppData() {
         final AppData appData = new AppData();
         appData.setDate(new Date());
         appData.setVersion("v1");
+        appData.setEnv(env);
         return appData;
     }
 }
@@ -35,4 +40,5 @@ class HelloController {
 class AppData {
     private Date date;
     private String version;
+    private String env;
 }
